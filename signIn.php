e5e223a9722498a0d3d98aed6e5ea65e1fae21ec
ishkining <ishkining@mail.ru>
2018-11-01 12:45:11 +0500

    Update signIn.php

---
+++ signIn.php
@@ -0,0 +1,10 @@
+<?
+  require "db/connection.php";
+?>
+
+<div>
+  <form method="POST">
+    <input name='login' type="text" placeholder="username" />
+    <input name="password" type="password" placeholder="password"/>
+  </form>
+</div>
