fa0f33026484079e056bc181288b8f9e6a4f8986
Zudwa-8 <34678214+Zudwa-8@users.noreply.github.com>
2018-11-01 20:59:36 +0500

    Update db/connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,15 +1,15 @@
 <?
-  session_start();
+  session_start(); // Открытие сессии
   $mysqli = new mysqli("localhost", "root", "", "ComputerConsuming");
 	$mysqli->query("SET NAMES 'utf8'");
-  function getFromDB($exampleDB)
+  function getFromDB($exampleDB) // Данная функция берет все данные с БД
 	{
 		while(($row = $exampleDB->fetch_assoc()) != false)
 		{
 			print_r($row);
 		}
 	}
-	function getFromAttribute($getRow, $attribute, $table)
+	function getFromAttribute($getRow, $attribute, $table) // Данная функция берет отдельно с какого именного свойства
 	{
 		global $mysqli;
 		$exampleDB = $mysqli->query("SELECT `".$attribute."` FROM `".$table."` ORDER BY `id`");
@@ -20,6 +20,9 @@
 		}
 		return $getRow;
 	}
+  /*
+    Данная функция показывает все товары в фиксированных блоках
+  */
   function showProducts($title, $image, $description, $price, $producer)
   {
     echo '<div style="float: left; background-color: green; width: 300px; height: 400px; margin: 10px;">
@@ -29,6 +32,9 @@
             <p>Price - '.$price.' ; Producer - '.$producer.'</p>
           </div>';
   }
+  /*
+    Данная функция отвечает за показ именно группировок товаров
+  */
   function showTypesOfProducts($title, $image)
   {
     echo '<div class="window_one">
