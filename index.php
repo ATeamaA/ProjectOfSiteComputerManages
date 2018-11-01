56ecfed7babba8177943b518012409ba95f33daa
Zudwa-8 <34678214+Zudwa-8@users.noreply.github.com>
2018-11-01 11:32:59 +0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,104 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Shop</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<a href="index.php"><img src="img/logo/logo.png" width="200px" alt="На главную"></a>
+		</div>
+		<div class="search">
+			<p>
+				<input class="search_string" type="text" name="search_query" placeholder="Поиск по сайту">
+				<input class="search_button" type="submit" value="Найти">	
+			</p>
+		</div>
+		<nav>
+			<div class="left_nav">
+				<a href="index.php">Главная</a>
+				<a href="*">Контакты</a>
+			</div>
+			<div class="right_nav">
+				<a href="*"><img src="img/logo/icons8-корзина-30.png" alt="Корзина"><p>Корзина</p></a>
+				<a href="*"><img src="img/logo/icons8-мужчина-пользователь-26.png" alt="Войти"><p>Вход</p></a>
+				<a href="*"><img src="img/logo/icons8-добавить-пользователя-filled-50.png" alt="Регистрация"><p>Регистрация</p></a>
+				<a href="*"></a>
+				<a href="*"></a>
+				<a href="*"></a>	
+			</div>
+		</nav>
+	</header>
+	<article>
+		<div class="list">
+			<h4>Категории</h4>
+			<div class="list_push">
+				<a href="*">Настольные ПК</a><br>
+			</div>
+			<div class="list_push">
+				<a href="*">Ноутбуки</a><br>
+			</div>
+			<div class="list_push">
+				<a href="*">Ультрабуки</a><br>
+			</div>
+			<div class="list_push">
+				<a href="*">Нетбуки</a><br>
+			</div>
+			<div class="list_push">
+				<a href="*">Планшеты</a><br>
+			</div>
+			<div class="list_push">
+				<a href="*">КПК</a><br>
+			</div>
+		</div>
+		<div class="content">
+			<div class="window_one">
+				<img src="img/" alt="Настольный Пк">
+			</div>
+			<div class="window_two">
+				<img src="img/" alt="Ноутбук">
+			</div>
+			<div class="window_three">
+				<img src="img/" alt="Ультрабук">
+			</div>
+			<div class="window_four">
+				<img src="img/" alt="Нетбук">
+			</div>
+			<div class="window_five">
+				<img src="img/" alt="Планшет">
+			</div>
+			<div class="window_six">
+				<img src="img/" alt="КПК">
+			</div>
+		</div>
+		<div class="banner">
+			<img src="img/logo/banner.jpg" alt="Спецпредложение">
+		</div>
+	</article>
+	<footer>
+		<div class="info">
+			<div class="info_one">
+				<h4>Новость</h4>
+				<p>Описание новости</p>
+			</div>
+			<div class="info_two">
+				<h4>Новость</h4>
+				<p>Описание новости</p>
+			</div>
+			<div class="info_three">
+				<h4>Новость</h4>
+				<p>Описание новости</p>
+			</div>
+			<div class="info_four">
+				<h4>Новость</h4>
+				<p>Описание новости</p>
+			</div>
+		</div>
+		<div class="creators">
+			<h5>2018 - Ишкинин Г.Г. & Тимохин А.М. 15П-2</h5>
+		</div>
+	</footer>
+</body>
+</html>
