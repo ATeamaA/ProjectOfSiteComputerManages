cd46587f219439a4974a9d923c347659db4d1dd8
ishkining <ishkining@mail.ru>
2018-11-30 11:22:40 +0500

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -31,7 +31,7 @@
             <div class="window_categories_text">
               <p align="justify" style="margin: 10px">'.substr($description, 0, 101).'...</p>
               <p align="justify" style="margin: 10px">Цена - '.$price.' ; Производство - '.$producer.'</p>
-            </div>  
+            </div>
           </div>';
   }
   /*
@@ -52,5 +52,33 @@
             <p style="margin: 10px">Кто отправил - '.$login.'</p>
             <input style="margin: 10px" type="submit" value="Натравить собак" />
           </div>';
+  }
+
+  function addFavorite($idUser, $idProduct)
+  {
+  	global $mysqli;
+    $mysqli->query("INSERT INTO `favorites` (`login`, `idProduct`) VALUES ('".$idUser."', '".$idProduct."')");
+  }
+
+  function showFavorite($title, $image, $description, $price, $producer, $id)
+  {
+    echo '
+      		  <div class="window_product">
+              <h2 style="margin: 10px"><a href="index.php?id='.$id.'">'.$title.'</a></h2>
+              <img style="margin: 10px" src= "'.$image.'" width: "100px" height="100px">
+              <div class="window_categories_text">
+                	<p align="justify" style="margin: 10px">'.substr($description, 0, 101).'...</p>
+                	<p align="justify" style="margin: 10px">Цена - '.$price.' ; Производство - '.$producer.'</p>
+              </div>
+  			       <form class="dislike_btn" method="POST">
+      		 		<button type="submit" name="'.$id.'"><img src="img/logo/dislike.png"></button>
+      			</form>
+      	    </div>';
+  }
+  function deleteFavorite($id)
+  {
+    global $mysqli;
+    $mysqli->query("DELETE FROM `favorites` WHERE `login` = '".$_SESSION['logged_user'][0]."' and `idProduct` = $id");
+    echo '<script>document.location.href="index.php?favorite=work"</script>';
   }
 ?>
